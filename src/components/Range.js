0b857a53e748f5101e9d5967c3d259135f88da10
ThakareSakshi <sakshithakare121@gmail.com>
2023-12-25 22:39:56 +0530

    added range input

--- src/components/Range.js
+++ src/components/Range.js
@@ -2,30 +2,98 @@ import React from 'react'
 import { useState } from 'react';
 
 const Range = () => {
-    const [homeValue, setHomeValue] = useState(50);
-    const [downPayment,setDowmPayment]=useState(0);
-    const [loanAmount,setLoanAmount]=useState(0);
+    const [homeValue, setHomeValue] = useState(3000);
+    const [downPayment,setDowmPayment]=useState(1000);
+    const [loanAmount,setLoanAmount]=useState(2000);
     const [interest,setInterest]=useState(2);
 
   const handleHomeChange = (event) => {
     setHomeValue(event.target.value);
   };
+
+  const handlePaymentChange = (event) => {
+    setDowmPayment(event.target.value);
+    setLoanAmount(homeValue-event.target.value)
+  };
+
+  const handleLoanChange = (event) => {
+    setDowmPayment(homeValue-event.target.value);
+    setLoanAmount(event.target.value)
+  };
+
+  const handleInterestChange = (event) => {
+    setInterest(event.target.value)
+  };
     return (
+        <>
         <div>
             <p>Home Value</p>
           <h1> $ {homeValue}</h1>
-          {/* <label htmlFor="rangeInput">Select a value:</label> */}
+         
           <input
             type="range"
-            id="rangeInput"
-            name="rangeInput"
+            id="homeinput"
+            name="homeInput"
             min="1000"
             max="10000"
             value={homeValue}
             onChange={handleHomeChange}
           />
-          {/* <p>Selected value: {rangeValue}</p> */}
+         
         </div>
+        
+        <div>
+            <p>Down Payment</p>
+          <h1> $ {downPayment}</h1>
+         
+          <input
+            type="range"
+            id="paymentInput"
+            name="paymentInput"
+            min="0"
+            max={homeValue}
+            value={downPayment}
+            onChange={handlePaymentChange}
+          />
+         
+        </div>
+        <div>
+            <p>Laon Amount</p>
+          <h1> $ {loanAmount}</h1>
+         
+          <input
+            type="range"
+            id="loanInput"
+            name="loanInput"
+            min="0"
+            max={homeValue}
+            value={loanAmount}
+            onChange={handleLoanChange}
+          />
+         
+        </div>
+        
+        
+        <div>
+            <p>Interest Rate</p>
+          <h1> {interest} %</h1>
+         
+          <input
+            type="range"
+            id="interestInput"
+            name="interestInput"
+            min="2"
+            max="18"
+            value={interest}
+            onChange={handleInterestChange}
+          />
+         
+        </div>
+        
+        
+       
+        
+        </>
       );
 }
 
